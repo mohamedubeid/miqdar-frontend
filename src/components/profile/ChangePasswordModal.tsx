37c2b1580a6e6aabc827f3846ba1630ed88a3425
mohamedubeid <mohammed.ubeid99@gmail.com>
2025-06-10 10:41:53 +0300

    edit change password modal style

--- src/components/profile/ChangePasswordModal.tsx
+++ src/components/profile/ChangePasswordModal.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useRef, useState } from "react";
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { Eye, EyeOff, Info, LockKeyhole } from "lucide-react";
 
 const ChangePasswordModal = () => {
@@ -35,16 +35,13 @@ const ChangePasswordModal = () => {
         <LockKeyhole size={16} />
         <span> تغيير كلمة المرور</span>
       </DialogTrigger>
-      <DialogContent className="max-w-[706px] sm:max-w-[706px] w-full rounded-[38px] p-11 max-h-[calc(100vh-2rem)] overflow-auto py-10">
+      <DialogContent className="max-w-[706px] sm:max-w-[706px] w-full rounded-[38px] p-11 py-10">
         <DialogHeader>
           <DialogTitle className="text-center text-2xl">تغيير كلمة المرور</DialogTitle>
-          <DialogDescription className="text-center text-cstm-gray">
-            يمكنك تغيير كلمة المرور الخاصة بك هنا. تأكد من اختيار كلمة مرور قوية وصعبة التخمين.
-          </DialogDescription>
         </DialogHeader>
-        <form className="space-y-6" onSubmit={handleSubmit}>
+        <form className="space-y-2 max-h-[calc(100vh-4rem)]  overflow-auto" onSubmit={handleSubmit}>
           <div className="relative">
-            <label className="text-xl text-cstm-gray block mb-3">كلمة المرور الحالية</label>
+            <label className="text-xl text-cstm-gray block mb-2">كلمة المرور الحالية</label>
             <input
               type={show.current ? "text" : "password"}
               id="currentPassword"
@@ -56,7 +53,7 @@ const ChangePasswordModal = () => {
             />
             <button
               type="button"
-              className="absolute top-[60px] left-4 text-gray-400"
+              className="absolute top-[55px] left-4 text-gray-400"
               tabIndex={-1}
               onClick={() => setShow((s) => ({ ...s, current: !s.current }))}
             >
@@ -64,7 +61,7 @@ const ChangePasswordModal = () => {
             </button>
           </div>
           <div className="relative">
-            <label className="text-xl text-cstm-gray block mb-3">كلمة المرور الجديدة</label>
+            <label className="text-xl text-cstm-gray block mb-2">كلمة المرور الجديدة</label>
             <input
               type={show.new ? "text" : "password"}
               id="newPassword"
@@ -76,7 +73,7 @@ const ChangePasswordModal = () => {
             />
             <button
               type="button"
-              className="absolute top-[60px] left-4 text-gray-400"
+              className="absolute top-[55px] left-4 text-gray-400"
               tabIndex={-1}
               onClick={() => setShow((s) => ({ ...s, new: !s.new }))}
             >
@@ -84,7 +81,7 @@ const ChangePasswordModal = () => {
             </button>
           </div>
           <div className="relative">
-            <label className="text-xl text-cstm-gray block mb-3">تأكيد كلمة المرور الجديدة</label>
+            <label className="text-xl text-cstm-gray block mb-2">تأكيد كلمة المرور الجديدة</label>
             <input
               type={show.confirm ? "text" : "password"}
               id="confirmNewPassword"
@@ -96,18 +93,18 @@ const ChangePasswordModal = () => {
             />
             <button
               type="button"
-              className="absolute top-[60px] left-4 text-gray-400"
+              className="absolute top-[55px] left-4 text-gray-400"
               tabIndex={-1}
               onClick={() => setShow((s) => ({ ...s, confirm: !s.confirm }))}
             >
               {show.confirm ? <EyeOff size={20} /> : <Eye size={20} />}
             </button>
           </div>
-          <div className="flex items-center gap-x-3 bg-[#FEF9C35E] px-8 rounded-[15px]">
+          <div className="flex items-center gap-x-3 bg-[#FEF9C35E] px-8 rounded-[15px] mt-3">
             <Info size={96} strokeWidth={1} className="text-[#CA8A04]" />
             <p className="text-[#CA8A04]"> كلمة المرور يجب أن تحتوي على 8 أحرف على الأقل، وتتضمن أحرف كبيرة وصغيرة وأرقام ورموز خاصة</p>
           </div>
-          <div className="flex gap-6 mt-8">
+          <div className="flex gap-6 mt-4">
             <button type="submit" className="primary-button w-full">
               حفظ التغييرات
             </button>
