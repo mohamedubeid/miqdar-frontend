12f728a6c45685e21929b136485d370960a1ba52
mohamedubeid <mohammed.ubeid99@gmail.com>
2025-06-25 19:53:25 +0300

    add hostingersite.com to next.config.js

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
+        protocol: 'https',
+        hostname: 'olivedrab-hyena-876790.hostingersite.com',
+        pathname: '/storage/**',
+      },
+      {
         protocol: 'http',
         hostname: '127.0.0.1',
         port: '8000',
