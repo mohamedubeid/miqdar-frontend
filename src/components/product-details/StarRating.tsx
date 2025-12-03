cfbc96cb878908e4d83d0b93e0e59c8c2264e0ba
mohamedubeid <mohammed.ubeid99@gmail.com>
2025-12-03 17:06:14 +0200

    increase rate stars size

--- src/components/product-details/StarRating.tsx
+++ src/components/product-details/StarRating.tsx
@@ -30,9 +30,9 @@ const StarRating: React.FC<StarRatingProps> = ({
   const clampedRating = Math.max(0, Math.min(numericRating, maxStars));
 
   const sizeClasses = {
-    sm: 'w-3 h-3',
-    md: 'w-4 h-4',
-    lg: 'w-5 h-5'
+    sm: 'w-4 h-4',
+    md: 'w-5 h-5',
+    lg: 'w-6 h-6'
   };
 
   const starSize = sizeClasses[size];
